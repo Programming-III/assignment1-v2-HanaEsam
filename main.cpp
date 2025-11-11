8cb563a6e43e342a9080bc563f4967ef0d285348
HanaEsam <hana.207118@gmail.com>
2025-11-11 09:47:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,74 @@
 #include <iostream>
 #include <string>
 #include "Person.h"
-#include "Student.h"
+#include "student.h"
 #include "Instructor.h"
 #include "Course.h"
-
-
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+
+
+string Person::getName(){
+    return name;
+
+}
+int Person::getId(){
+    return id;
+}
+
+void Person::setName(string s){
+    name=s;
+}
+
+void Person ::setId(int x){
+   id=x;
+}
+void Person::display(){
+cout<< "Name: "<< getName()<< endl;
+cout<< "Id: "<< getId()<< endl;
+
+}
+
+void student::setMajor(string m){
+major =m;
+}
+
+void student ::setYear(int x){
+yearLevel=x;
+}
+string student::getMajor(){
+    return major;
+
+}
+int student::getYL(){
+    return yearLevel;
+}
+
+///void student::display() : Person {
+///    cout<<"Year Level: "<< getYL()<<endl;
+ ///   cout << "Major: "<< getMajor()<< endl;
+
+///}
 
 
 
 
 
 
+void Course::displayCourseInfo(){
+cout<< courseCode<< courseName << maxStudents<< endl;
+}
 
 
-// ==================== Student Class Implementation ====================
+int main(){
+ student s;
+ s.setName("Omar Nabil");
 
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
+ s.setYear(2);
+ s.setMajor("Informatics");
+ Instructor i;
+ i.setName("Dr.Lina Khaled");
 
 
 
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
 }
